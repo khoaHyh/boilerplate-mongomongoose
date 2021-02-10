092313adb3b42de495c6a66fcd9ee8f1837fcd21
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-09 22:17:14 -0500

    Fix: Tests time out
    Add logging for mongoose connect to try and see if connecting to the db
    is the problem.

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) console.log('error connecting to db, ' + err);
+});
 // mongoose.connect(process.env.MONGO_URI);
 
 const personSchema = new Schema({
